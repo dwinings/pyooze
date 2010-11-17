2aede764c4b3d9ce68cafb1fc268ad059ea03f97
mopherad <Ianinchaos@gmail.com>
2010-11-17 16:43:33 -0500

    added dirt1

--- loader.py
+++ loader.py
@@ -5,5 +5,6 @@ class Resources:
     grass1tex = pygame.image.load("Art/grass1.png")
     grass2tex = pygame.image.load("Art/grass2.png")
     defaulttex = pygame.image.load("Art/default.png")
+    dirt1tex = pygame.image.load("Art/dirt1.png")
 
 
