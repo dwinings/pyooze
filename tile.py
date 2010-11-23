5d3daba3d04bd696b020db88ef0ed8803d17e65e
mopherad <Ianinchaos@gmail.com>
2010-11-22 23:19:15 -0500

    added scable random block

--- tile.py
+++ tile.py
@@ -5,28 +5,26 @@ class Tile(pygame.sprite.Sprite):
     passable = False
 
     #Image and rect are both required by the sprite/group subsystem
+    #Images are put in a list,  and randomly picked on creation
+    imageList = [Resources.defaulttex]
     image = Resources.defaulttex
     
     def __init__(self, x = 0.0, y = 0.0):
+        self.pickTile()
         pygame.sprite.Sprite.__init__(self)
         self.position = (float(x), float(y))
         self.rect = self.image.get_rect(topleft=self.position)
 
     def __repr__(self):
         return str(self.position)
+    
+    def pickTile(self):
+        randomPick = random.randrange(0,len(self.imageList))
+        self.image = self.imageList[randomPick]
 
 class Grass(Tile):
     passable = True
-    image = Resources.grass2tex
-    choice = 0
+    imageList = [Resources.grass1tex,Resources.grass2tex]
+    
     def __init__(self, x = 0.0, y = 0.0):
-        Grass.pickTile(self)
         Tile.__init__(self,x,y)
-
-    def pickTile(self):
-        choice = random.randrange(0,2)
-        if choice == 0:
-            self.image = Resources.grass1tex
-        else:
-            self.image = Resources.grass2tex
-
